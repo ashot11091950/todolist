4ef360df77d73939c4c23174d3a500da70b2fe2d
ashot5706 <ashot.hayrapetyan.2001@mail.ru>
2019-01-30 11:52:41 +0400

    delete mvc

---
+++ classes.php
@@ -0,0 +1,26 @@
+<?php 
+	class Model{
+		public function __contruct(){
+			$con = mysqli_connect('localhost', 'root', '@,~rXtn3hU5?3sn~', 'todo_bs');
+			mysqli_set_charset($connect,"utf8");
+			return $con;
+		}
+	}
+	class Controller{
+		
+	}
+	class DB{
+		protected function insert($table,Array $data){
+
+		}
+		protected function select($table,Array $data){
+			
+		}
+		protected function update($table,Array $data){
+			
+		}
+		protected function delete($table,Array $data){
+			
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	include 'config.php';
+	if(DEBUG){
+		ini_set('display_errors', true);
+		error_reporting(E_ALL);
+	}
+?>
