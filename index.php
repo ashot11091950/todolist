41cf606bacb7c2cfed95ad417cef13109567966f
ashot5706 <ashot.hayrapetyan.2001@mail.ru>
2019-03-08 00:33:40 +0400

    comments

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 	include 'config.php';
+
+	//error handling
 	if(DEBUG){
 		ini_set('display_errors', true);
 		error_reporting(E_ALL);
 	}else{
 		ini_set('display_errors', false);
 	}
+
+	//start engine
 	include 'vendor/startup.php';
